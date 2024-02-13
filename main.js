172d3e1e325de9616ad021b3fec1a12cbbdc5516
Tkumbasar <turan-kumbasar@live.fr>
2024-02-13 15:17:55 +0100

    portfolio-debut

---
+++ main.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById('toggleButton');
+const body = document.body;
+
+toggleButton.addEventListener('click', function() {
+  body.classList.toggle('dark-mode');
+});
+window.addEventListener('scroll', function() {
+    var scrollToTop = document.getElementById('scrollToTop');
+    if (window.scrollY > 100) {
+        scrollToTop.style.display = 'block';
+    } else {
+        scrollToTop.style.display = 'none';
+    }
+});
+
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
