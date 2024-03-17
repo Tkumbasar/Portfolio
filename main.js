282b385e3064de037dab848189bfe1c9a114a679
Tkumbasar <turan-kumbasar@live.fr>
2024-03-17 15:30:42 +0100

    pf terminer

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-const toggleButton = document.getElementById('toggleButton');
-const body = document.body;
+function randomizeWidthAndColor() {
+    var growingLine = document.getElementById("growingLine","growingLine1");
+    var randomWidth = Math.floor(Math.random() * 900) + 200; // Largeur aléatoire entre 50 et 250 pixels
+    growingLine.style.width = randomWidth + "px";
+  
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Générer une couleur hexadécimale aléatoire
+    growingLine.style.backgroundColor = randomColor;
+  }
+  
+  setInterval(randomizeWidthAndColor, 1000); // Appel de la fonct
 
-toggleButton.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
-});
-window.addEventListener('scroll', function() {
-    var scrollToTop = document.getElementById('scrollToTop');
-    if (window.scrollY > 100) {
-        scrollToTop.style.display = 'block';
+
+  window.addEventListener('scroll', function() {
+    var button = document.getElementById('fixedButton');
+    if (window.scrollY > 0) {
+      button.classList.add('visible');
     } else {
-        scrollToTop.style.display = 'none';
+      button.classList.remove('visible');
     }
-});
-
-document.getElementById('scrollToTop').addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-
-
-// image sous la nav
-
-
+  });
+  
+  document.getElementById("fixedButton").addEventListener("click", function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  });
